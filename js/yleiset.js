1d8b80fab1bf2f2b1b24b7eac4fbb1d07d74f995
karkuu <36222082+karkuu@users.noreply.github.com>
2018-02-20 12:09:44 +0200

    Isännöitsijä henk. yht. JS

--- js/yleiset.js
+++ js/yleiset.js
@@ -10,7 +10,7 @@ class dataHaku
 		this.postinumero = "345343";
 		this.postitoimipaikka = "Testaamo";
 		this.sahkoposti = "teppo@testaaja.nulll";
-		this.ytunnus = "";
+		this.ytunnus = "Y-123456567";
 		
 	}
 	maaritaSolujenMaara(maara)
@@ -32,17 +32,18 @@ class dataHaku
 	{
 		var sisalto = "";
 		var curStyle = ""
+		var elem,displayStyle
 		
 		document.getElementById("isannoitsijaHenkilotHenkilotd"+kenttaID).innerHTML = '<h4>Teppo Testaaja</h4><form><div class="form-row"><div class="form-group col-md-3"><label for="formGroupNimi">Nimi</label><input type="text" class="form-control" id="formGroupNimi" value="' + this.nimi + '"></div><div class="form-group col-md-3"><label for="formGroupNimi">Sukunimi</label><input type="text" class="form-control" id="formGroupNimi" value="' + this.sukunimi + '"></div></div><div class="form-row"><div class="form-group col-md-6"><label for="formGroupOsoite">Osoite</label><input type="text" class="form-control" id="formGroupOsoite" value="' + this.osoite + '"></div></div><div class="form-row"><div class="form-group col-md-2"><label for="formGroupPostinumero">Postinumero</label><input type="text" class="form-control" id="formGroupPostinumero" value="' + this.postinumero + '"></div><div class="form-group col-md-4"><label for="formGroupPostinumero">Postitoimipaikka</label><input type="text" class="form-control" id="formGroupPostinumero" value="' + this.postitoimipaikka + '"></div></div><div class="form-row"><div class="form-group col-md-6"><label for="kayttajaInputEmail1">Sähköposti</label><input type="email" class="form-control" id="kayttajaInputEmail1" aria-describedby="emailHelp" value="' + this.sahkoposti + '"><small id="emailHelp" class="form-text text-muted">Emme tee osoitteellasi mitään laitonta.</small></div></div><button type="submit" class="btn btn-primary">Tallenna</button> <button type="button" class="btn btn-primary">Poista käyttäjä</button></form>';
 		
-		var elem = document.getElementById("isannoitsijaHenkilotHenkilo"+kenttaID);
+		elem = document.getElementById("isannoitsijaHenkilotHenkilo"+kenttaID);
 		if (elem.currentStyle)
 		{
-			var displayStyle = elem.currentStyle.display;
+			displayStyle = elem.currentStyle.display;
 		}
 		else if (window.getComputedStyle)
 		{
-			var curStyle = window.getComputedStyle(elem, null).getPropertyValue("display");
+			curStyle = window.getComputedStyle(elem, null).getPropertyValue("display");
 		}
 		
 		if (curStyle == "none")
@@ -62,6 +63,43 @@ class dataHaku
 				}
 		}
 	}
+	naytaYhtionTiedot(kenttaID)
+	{
+		var sisalto = "";
+		var curStyle = ""
+		var elem,displayStyle
+		
+		document.getElementById("isannoitsijaYhtiotYhtiotd"+kenttaID).innerHTML = '<h4>As. Oy Maailman paras talonyhtiö</h4><form><div class="form-row"><div class="form-group col-md-4"><label for="formGroupNimi">Nimi</label><input type="text" class="form-control" id="formGroupNimi" value="' + this.nimi + '"></div><div class="form-group col-md-2"><label for="formGroupNimi">Y-tunnus</label><input type="text" class="form-control" id="formGroupYtunnus" value="' + this.ytunnus + '"></div></div><div class="form-row"><div class="form-groupform-group3 col-md-6"><label for="formGroupOsoite">Osoite</label><input type="text" class="form-control" id="formGroupOsoite" value="' + this.osoite + '"></div></div><div class="form-row"><div class="form-group col-md-2"><label for="formGroupPostinumero">Postinumero</label><input type="text" class="form-control" id="formGroupPostinumero" value="' + this.postinumero + '"></div><div class="form-group col-md-4"><label for="formGroupPostinumero">Postitoimipaikka</label><input type="text" class="form-control" id="formGroupPostinumero" value="' + this. postitoimipaikka + '"></div></div><button type="submit" class="btn btn-primary">Tallenna</button> <button type="button" class="btn btn-primary">Poista yhtiö</button></form>';
+		
+		elem = document.getElementById("isannoitsijaYhtiotYhtio"+kenttaID);
+		if (elem.currentStyle)
+		{
+			displayStyle = elem.currentStyle.display;
+		}
+		else if (window.getComputedStyle)
+		{
+			curStyle = window.getComputedStyle(elem, null).getPropertyValue("display");
+		}
+		
+		if (curStyle == "none")
+		{
+			document.getElementById("isannoitsijaYhtiotYhtio"+kenttaID).style.display = "table-row";
+		}
+		else if (curStyle == "table-row")
+		{
+			document.getElementById("isannoitsijaYhtiotYhtio"+kenttaID).style.display = "none";
+		}
+		
+		for (var i=1;i<this.kasiteltaviasoluja;i++)
+		{
+				if (i != kenttaID)
+				{
+					document.getElementById("isannoitsijaYhtiotYhtio"+i).style.display = "none";
+				}
+		}
+	}
+	
+	
 	
 	
 }
