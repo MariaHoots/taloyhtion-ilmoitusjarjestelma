847086b2d8910202f4f62311b7adef7c1e2290e6
karkuu <36222082+karkuu@users.noreply.github.com>
2018-02-20 11:45:52 +0200

    Tietojen näyttäminen

--- js/yleiset.js
+++ js/yleiset.js
@@ -11,6 +11,7 @@ class dataHaku
 		this.postitoimipaikka = "Testaamo";
 		this.sahkoposti = "teppo@testaaja.nulll";
 		this.ytunnus = "";
+		
 	}
 	maaritaSolujenMaara(maara)
 	{
@@ -29,42 +30,37 @@ class dataHaku
 	
 	naytaHenkilonTiedot(kenttaID)
 	{
-		var i;
 		var sisalto = "";
-		for (i=1;i<this.kasiteltaviasoluja;i++)
+		var curStyle = ""
+		
+		document.getElementById("isannoitsijaHenkilotHenkilotd"+kenttaID).innerHTML = '<h4>Teppo Testaaja</h4><form><div class="form-row"><div class="form-group col-md-3"><label for="formGroupNimi">Nimi</label><input type="text" class="form-control" id="formGroupNimi" value="' + this.nimi + '"></div><div class="form-group col-md-3"><label for="formGroupNimi">Sukunimi</label><input type="text" class="form-control" id="formGroupNimi" value="' + this.sukunimi + '"></div></div><div class="form-row"><div class="form-group col-md-6"><label for="formGroupOsoite">Osoite</label><input type="text" class="form-control" id="formGroupOsoite" value="' + this.osoite + '"></div></div><div class="form-row"><div class="form-group col-md-2"><label for="formGroupPostinumero">Postinumero</label><input type="text" class="form-control" id="formGroupPostinumero" value="' + this.postinumero + '"></div><div class="form-group col-md-4"><label for="formGroupPostinumero">Postitoimipaikka</label><input type="text" class="form-control" id="formGroupPostinumero" value="' + this.postitoimipaikka + '"></div></div><div class="form-row"><div class="form-group col-md-6"><label for="kayttajaInputEmail1">Sähköposti</label><input type="email" class="form-control" id="kayttajaInputEmail1" aria-describedby="emailHelp" value="' + this.sahkoposti + '"><small id="emailHelp" class="form-text text-muted">Emme tee osoitteellasi mitään laitonta.</small></div></div><button type="submit" class="btn btn-primary">Tallenna</button> <button type="button" class="btn btn-primary">Poista käyttäjä</button></form>';
+		
+		var elem = document.getElementById("isannoitsijaHenkilotHenkilo"+kenttaID);
+		if (elem.currentStyle)
 		{
-			if (i != kenttaID)
-			{
-				document.getElementById("isannoitsijaHenkilotHenkilo"+i).style.display = "none";
-			}
+			var displayStyle = elem.currentStyle.display;
+		}
+		else if (window.getComputedStyle)
+		{
+			var curStyle = window.getComputedStyle(elem, null).getPropertyValue("display");
 		}
 		
-		if (document.getElementById("isannoitsijaHenkilotHenkilo"+kenttaID).style.display == "none")
+		if (curStyle == "none")
 		{
 			document.getElementById("isannoitsijaHenkilotHenkilo"+kenttaID).style.display = "table-row";
 		}
-		else if (document.getElementById("isannoitsijaHenkilotHenkilo"+kenttaID).style.display == "table-row")
+		else if (curStyle == "table-row")
 		{
 			document.getElementById("isannoitsijaHenkilotHenkilo"+kenttaID).style.display = "none";
 		}
 		
-		document.getElementById("isannoitsijaHenkilotHenkilotd"+kenttaID).innerHTML = '<h4>Teppo Testaaja</h4><form><div class="form-row"><div class="form-group col-md-3"><label for="formGroupNimi">Nimi</label><input type="text" class="form-control" id="formGroupNimi" value="' + this.nimi + '"></div><div class="form-group col-md-3"><label for="formGroupNimi">Sukunimi</label><input type="text" class="form-control" id="formGroupNimi" value="' + this.sukunimi + '"></div></div><div class="form-row"><div class="form-group col-md-6"><label for="formGroupOsoite">Osoite</label><input type="text" class="form-control" id="formGroupOsoite" value="' + this.osoite + '"></div></div><div class="form-row"><div class="form-group col-md-2"><label for="formGroupPostinumero">Postinumero</label><input type="text" class="form-control" id="formGroupPostinumero" value="' + this.postinumero + '"></div><div class="form-group col-md-4"><label for="formGroupPostinumero">Postitoimipaikka</label><input type="text" class="form-control" id="formGroupPostinumero" value="' + this.postitoimipaikka + '"></div></div><div class="form-row"><div class="form-group col-md-6"><label for="kayttajaInputEmail1">Sähköposti</label><input type="email" class="form-control" id="kayttajaInputEmail1" aria-describedby="emailHelp" value="' + this.sahkoposti + '"><small id="emailHelp" class="form-text text-muted">Emme tee osoitteellasi mitään laitonta.</small></div></div><button type="submit" class="btn btn-primary">Tallenna</button> <button type="button" class="btn btn-primary">Poista käyttäjä</button></form>';
-			
-			
-			
-		
-		
-		
-		
-		
-		
-		
-	//	document.getElementById("isannoitsijaHenkilotHenkilo"+kenttaID).childNodes[0].value = "aaa";
-		
-		
-		
-		
-		
+		for (var i=1;i<this.kasiteltaviasoluja;i++)
+		{
+				if (i != kenttaID)
+				{
+					document.getElementById("isannoitsijaHenkilotHenkilo"+i).style.display = "none";
+				}
+		}
 	}
 	
 	
