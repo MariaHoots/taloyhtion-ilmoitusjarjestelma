9511ce245eb4292cbf13bfd1f88798e7a1f5f763
MariaHoots <maria.mk@protonmail.com>
2018-03-22 13:05:35 +0200

    lisätty oikeat linkit (henkilöt ja ilmoitukset) ja poistettu väärä icon

--- dev/frontend/src/container/components/IsannoitsijaTaloyhtiotLista.js
+++ dev/frontend/src/container/components/IsannoitsijaTaloyhtiotLista.js
@@ -7,7 +7,7 @@ export default class EtuSivu extends React.Component {
 		let listView = {}
 
 		if (this.props.housingCompList.length === 0) {
-			listView = <td colSpan="8"><p>Ei taloyhtiöitä listassa</p></td>
+			listView = <tr><td colSpan="8"><p>Ei taloyhtiöitä listassa</p></td></tr>
 		} else {
 			listView = this.props.housingCompList.map((housingComp) =>
 				<tr key={housingComp._id}>
@@ -15,9 +15,8 @@ export default class EtuSivu extends React.Component {
 				<td colSpan="3" onClick="datahaku.naytaYhtionTiedot(1);"><a href="index.html">{housingComp.name}</a></td>
 				<td colSpan="3">{housingComp.address}, {housingComp.city}</td>
 				<td>
-					<a href="isannoitsija-henkilot.html"><img src="img/hallitus.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a>
-					<a href="isannoitsija-henkilot.html"><img src="img/henkilot.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a>
-					<a href="isannoitsija-ilmoitukset.html"><img src="img/ilmoitukset.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a>
+					<a href="/admin_henkilot"><img src="img/henkilot.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a>
+					<a href="/admin_ilmoitukset"><img src="img/ilmoitukset.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a>
 					<a href="asetukset.html"><img src="img/asetukset.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a>
 				</td>
 				</tr>
