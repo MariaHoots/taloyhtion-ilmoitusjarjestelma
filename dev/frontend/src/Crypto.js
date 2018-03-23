f29563b50f81f33c070521fb97acd7e007168a97
karkuu <36222082+karkuu@users.noreply.github.com>
2018-03-23 12:55:18 +0200

    Turha konstruktori pois

--- dev/frontend/src/Crypto.js
+++ dev/frontend/src/Crypto.js
@@ -2,16 +2,10 @@ let cr = require("crypto")
 
 export default class Crypto
 {
-    constructor()
-	{
-
-    }
-
     returnHash(Str) {
         let crypto = cr.createHash('sha256');
         crypto.update(Str);
         
         return crypto.digest('hex');
     }
-
 }
