7f593e01fcee4808daf21c3c982ac3de1e6386d4
MariaHoots <maria.mk@protonmail.com>
2018-03-23 11:54:08 +0200

    added modal popups to housing companies list

--- dev/frontend/src/container/components/IsannoitsijaTaloyhtiot.js
+++ dev/frontend/src/container/components/IsannoitsijaTaloyhtiot.js
@@ -2,45 +2,43 @@ import React from 'react';
 
 import IsannoitsijaTaloyhtiotLista from './IsannoitsijaTaloyhtiotLista';
 
-export default class EtuSivu extends React.Component
-{
+export default class EtuSivu extends React.Component {
 
 	componentDidMount() {
-		this.props.getHousingCompanies();	
+		this.props.getHousingCompanies();
 	}
 
-
-	render()
-	{
+	render() {
 		return (
 			<div className="card">
 			  <div className="card-body">
-			   <h3>Talonyhtiöt</h3>
-			  <table className="table table-bordered">
-				<thead>
-				  <tr>
-						<th scope="row"></th>
-						<th scope="row" colSpan="3">Yhtiön nimi</th>
-						<th scope="row" colSpan="3">Osoite</th>
-						<th scope="row">Toiminnot</th>
-				 </tr>
-				</thead>
-				<thead>
-				<tr>
-				<th scope="row"><a href="index.html"><img src="img/sort.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a></th>
-				<td><input type="text" className="form-control" id="yhtiotHaeYhtionNimellä" placeholder="Yhtiön nimi"/></td><td><a href="index.html"><img src="img/jarjestysylos.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a></td><td><a href="index.html"><img src="img/jarjestysalas.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a></td>
-				<td><input type="text" className="form-control" id="yhtiotHaeOsoitteella" placeholder="Osoite"/></td><td><a href="index.html"><img src="img/jarjestysylos.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a></td><td><a href="index.html"><img src="img/jarjestysalas.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a></td>
+			   	<h3>Talonyhtiöt</h3>
+				  <table className="table table-bordered">
+					<thead>
+					  <tr>
+							<th scope="row"></th>
+							<th scope="row" colSpan="3">Yhtiön nimi</th>
+							<th scope="row" colSpan="3">Osoite</th>
+							<th scope="row">Toiminnot</th>
+					 </tr>
+					</thead>
+					<thead>
+					<tr>
+					<th scope="row"><a href="index.html"><img src="img/sort.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a></th>
+					<td><input type="text" className="form-control" id="yhtiotHaeYhtionNimellä" placeholder="Yhtiön nimi"/></td><td><a href="index.html"><img src="img/jarjestysylos.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a></td><td><a href="index.html"><img src="img/jarjestysalas.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a></td>
+					<td><input type="text" className="form-control" id="yhtiotHaeOsoitteella" placeholder="Osoite"/></td><td><a href="index.html"><img src="img/jarjestysylos.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a></td><td><a href="index.html"><img src="img/jarjestysalas.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a></td>
 
 
-				<td></td>
-				</tr>
-				</thead>
+					<td></td>
+					</tr>
+					</thead>
 
-				<IsannoitsijaTaloyhtiotLista housingCompList={this.props.housingCompList}/>
+					<IsannoitsijaTaloyhtiotLista housingCompList={this.props.housingCompList}/>
 
-			  </table>
-			  </div>
+				  </table>
+				</div>
 			</div>
+
 		);
 	}
 }
--- dev/frontend/src/container/components/IsannoitsijaTaloyhtiotLista.js
+++ dev/frontend/src/container/components/IsannoitsijaTaloyhtiotLista.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
+
 export default class EtuSivu extends React.Component {
+
+
 
 	render() {
 
@@ -10,20 +13,74 @@ export default class EtuSivu extends React.Component {
 			listView = <tr><td colSpan="8"><p>Ei taloyhtiöitä listassa</p></td></tr>
 		} else {
 			listView = this.props.housingCompList.map((housingComp) =>
-				<tr key={housingComp._id}>
-				<th scope="row">*</th>
-				<td colSpan="3" onClick="datahaku.naytaYhtionTiedot(1);"><a href="index.html">{housingComp.name}</a></td>
-				<td colSpan="3">{housingComp.address}, {housingComp.city}</td>
-				<td>
-					<a href="/admin_henkilot"><img src="img/henkilot.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a>
-					<a href="/admin_ilmoitukset"><img src="img/ilmoitukset.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a>
-					<a href="asetukset.html"><img src="img/asetukset.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a>
-				</td>
+				<tr key={housingComp.id}>
+					<th scope="row">*</th>
+
+					<td colSpan="3">
+
+						<a href="#" data-toggle="modal" data-target={`#${housingComp.id}`}>{housingComp.name}</a>
+
+							<div className="modal fade" id={`${housingComp.id}`} tabIndex="-1" role="dialog" aria-labelledby={`${housingComp.name}`} aria-hidden="true">
+							  <div className="modal-dialog" role="document">
+							    <div className="modal-content">
+							      <div className="modal-header">
+							        <h5 className="modal-title" id={`${housingComp.name}`}>Muokkaa yhtiötä: {housingComp.name}</h5>
+							        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+							          <span aria-hidden="true">&times;</span>
+							        </button>
+							      </div>
+							      <div className="modal-body">
+
+											<form>
+											  <div className="form-group">
+											    <label for="name">Nimi</label>
+											    <input type="text" className="form-control" id="name" value={`${housingComp.name}`}></input>
+												</div>
+												<div className="form-group">
+											    <label for="address">Osoite</label>
+											    <input type="text" className="form-control" id="address" value={`${housingComp.address}`}></input>
+												</div>
+												<div className="form-group">
+											    <label for="zip">Postinumero</label>
+											    <input type="text" className="form-control" id="zip" value={`${housingComp.zip}`}></input>
+												</div>
+												<div classNameName="form-group">
+											    <label for="city">Kaupunki</label>
+											    <input type="text" className="form-control" id="city" value={`${housingComp.city}`}></input>
+												</div>
+												<div className="form-group">
+											    <label for="business_id">Y-tunnus</label>
+											    <input type="text" className="form-control" id="business_id" value={`${housingComp.business_id}`}></input>
+												</div>
+											</form>
+
+							      </div>
+							      <div className="modal-footer">
+							        <button type="button" className="btn btn-secondary" data-dismiss="modal">Sulje</button>
+							        <button type="button" className="btn btn-primary">Tallenna</button>
+							      </div>
+							    </div>
+							  </div>
+							</div>
+
+					</td>
+
+					<td colSpan="3">{housingComp.address}, {housingComp.city}</td>
+
+					<td>
+						<a href="/admin_henkilot"><img src="img/henkilot.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a>
+						<a href="/admin_ilmoitukset"><img src="img/ilmoitukset.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a>
+						<a href="#" data-toggle="modal" data-target={`#${housingComp.id}`}><img src="img/asetukset.svg" className="img-fluid" alt="[H]" height="20" width="20"/></a>
+					</td>
+
 				</tr>
 			)
 		}
 		return (
-			<tbody>{listView}</tbody>
+			<tbody>
+				{listView}
+			</tbody>
+
 		);
 	}
 }
