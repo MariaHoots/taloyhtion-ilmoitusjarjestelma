fa82a128eb7012b1f8da8dda6d813c804ee72348
jmseppala <jmseppala90@gmail.com>
2018-04-09 15:01:37 +0300

    Asetukset.js
    Pohjaa

---
+++ dev/frontend/src/container/components/Asetukset.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Asetukset from './Asetukset';
+
+export default class EtuSivu extends React.Component 
+/*
+{	
+
+	componentDidMount() {
+			this.props.getUsers();	
+	}
+
+	
+	render() {
+		return (
+			
+			  </div>
+			</div>
+		);
+	}
+} */
