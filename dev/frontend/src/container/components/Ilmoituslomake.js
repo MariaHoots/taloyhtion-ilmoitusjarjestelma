90ec21690ed9e3476385ed390520c549b3a523dc
MariaHoots <maria.mk@protonmail.com>
2018-04-18 13:44:31 +0300

    removed unused form lines

--- dev/frontend/src/container/components/Ilmoituslomake.js
+++ dev/frontend/src/container/components/Ilmoituslomake.js
@@ -6,57 +6,8 @@ export default class EtuSivu extends React.Component {
 		<div className="card">
 		  <div className="card-body">
 	      <h3>Ilmoituslomake</h3>
-        <p>Ole hyvä ja täytät uuden ilmoituksen tiedot alempana.</p>
+        <p>Hei {this.props.loggedUser.first_name}, voit jättää tässä uuden ilmoituksen.</p>
         <form>
-          <h4>Esitäytet tiedot</h4>
-          <br />
-          <div className="form-row">
-            <div className="form-group col-md-6">
-              <label htmlFor="etunimi">Etunimi</label>
-              <input type="text" className="form-control" id="etunimi" value={this.props.loggedUser.first_name} readOnly/>
-            </div>
-            <div className="form-group col-md-6">
-              <label htmlFor="sukunimi">Sukunimi</label>
-              <input type="text" className="form-control" id="sukunimi" value={this.props.loggedUser.last_name} readOnly/>
-            </div>
-          </div>
-          <div className="form-row">
-            <div className="form-group col-md-6">
-              <label htmlFor="taloyhtio">Taloyhtiö</label>
-              <input type="text" className="form-control" id="taloyhtio" placeholder=""/>
-            </div>
-            <div className="form-group col-md-6">
-              <label htmlFor="huoneisto">Huoneisto</label>
-              <input type="text" className="form-control" id="huoneisto" placeholder=""/>
-            </div>
-          </div>
-
-          <div className="form-row">
-            <div className="form-group col-md-6">
-              <label htmlFor="osoite">Osoite</label>
-              <input type="text" className="form-control" id="osoite" value={this.props.loggedUser.billing_address} readOnly/>
-            </div>
-            <div className="form-group col-md-4">
-              <label htmlFor="postinumero">Postinumero</label>
-              <input type="text" className="form-control" id="postinumero" value={this.props.loggedUser.zip} readOnly/>
-            </div>
-            <div className="form-group col-md-2">
-              <label htmlFor="postitoimipaikka">Postitoimipaikka</label>
-              <input type="text" className="form-control" id="postitoimipaikka" value={this.props.loggedUser.city} readOnly/>
-            </div>
-          </div>
-          <div className="form-row">
-            <div className="form-group col-md-6">
-              <label htmlFor="puhelin">Puhelin</label>
-              <input type="text" className="form-control" id="puhelin" value={this.props.loggedUser.phone} readOnly/>
-            </div>
-            <div className="form-group col-md-6">
-              <label htmlFor="sahkoposti">Sähköposti</label>
-              <input type="text" className="form-control" id="sahkoposti" value={this.props.loggedUser.email} readOnly/>
-            </div>
-          </div>
-          <h4>Ilmoituksen tiedot</h4>
-          <br />
           <div className="form-row">
             <div className="form-group col-md-6">
               <label htmlFor="taloyhtio">Ilmoituksen otsikko</label>
