5b204c70891fa5565f812f636c85e59fdf08ccee
jmseppala <jmseppala90@gmail.com>
2018-04-18 14:02:55 +0300

    Tiedot.js

--- dev/frontend/src/container/components/Tiedot.js
+++ dev/frontend/src/container/components/Tiedot.js
@@ -95,67 +95,37 @@ export default class EtuSivu extends React.Component {
 			<div class="form-row">
 				<div class="form-group col-md-3">
 					<label for="formGroupNimi">Nimi</label>
-					{/*<input type="text" 
-						   name="nimi" 
-						   class="form-control" 
-						   id="formGroupNimi"
-						   placeholder="Nimi" 
-						   value={this.state.nimi}
-						   onChange={this.onChange}/> */}	
+					{/*<input type="text" name="nimi" class="form-control" id="formGroupNimi" placeholder="Nimi" 
+					    value={this.state.nimi} onChange={this.onChange}/> */}	
 				</div>
 				<div class="form-group col-md-3">
 					<label for="formGroupNimi">Sukunimi</label>
-					{/*  <input type="text" 
-						   name="sukunimi"
-						   class="form-control" 
-						   id="formGroupNimi" 
-						   placeholder="Sukunimi"
-						   value={this.state.sukunimi} 
-						   onChange={this.onChange}/> */}
+					{/*<input type="text" name="sukunimi" class="form-control" id="formGroupNimi" placeholder="Sukunimi"
+					   value={this.state.sukunimi} onChange={this.onChange}/> */}
 				</div>
 			</div>
 				<div class="form-groupform-group">
 					<label for="formGroupOsoite">Osoite</label>
-					 {/*<input type="text"
-						   name="osoite"
-						   class="form-control"
-						   id="formGroupOsoite"
-						   placeholder="Osoite"
-						   value={this.state.osoite} 
-						   onChange={this.onChange}/> */}
+					 {/*<input type="text" name="osoite" class="form-control" id="formGroupOsoite" placeholder="Osoite"
+						value={this.state.osoite} onChange={this.onChange}/> */}
 			</div>
 			<div class="form-row">
 				<div class="form-group col-md-2">
 					<label for="formGroupPostinumero">Postinumero</label>
-					 {/*<input type="text"
-						   name="postinumero"
-						   class="form-control" 
-						   id="formGroupPostinumero" 
-						   placeholder="Postinumero"						   
-						   value={this.state.postinumero}
-						   onChange={this.onChange}/>  */}
+					 {/*<input type="text" name="postinumero" class="form-control" id="formGroupPostinumero" placeholder="Postinumero"						   
+						value={this.state.postinumero} onChange={this.onChange}/> */}
 				</div>
 				<div class="form-group col-md-4">
 					<label for="formGroupPostinumero">Postitoimipaikka</label>
-					{/*<input type="text" 
-						   name="postitoimipaikka"	
-						   class="form-control" 
-						   id="formGroupPostinumero"
-						   placeholder="Postitoimipaikka"
-						   value={this.state.postitoimipaikka} 
-						   onChange={this.onChange}/> */}
+					{/*<input type="text" name="postitoimipaikka" class="form-control" id="formGroupPostinumero" placeholder="Postitoimipaikka"
+					   value={this.state.postitoimipaikka} onChange={this.onChange}/> */}
 				</div>
 			</div>	
 			<div class="form-group">
 					<label for="kayttajaInputEmail1">Sähköposti</label>
-					{/* <input type="email"
-						   name="email" 
-						   class="form-control" 
-						   id="kayttajaInputEmail1" aria-describedby="emailHelp" 
-						   placeholder="Syötä sähköposti"/>
-						   <small id="emailHelp" class="form-text text-muted">Emme tee osoitteellasi mitään laitonta.</small> 
-						   value={this.state.email}
-						   onChange={this.onChange}/> */}	
+					{/* <input type="email" name="email" class="form-control" id="kayttajaInputEmail1" aria-describedby="emailHelp" 
+						 placeholder="Syötä sähköposti"/> <small id="emailHelp" class="form-text text-muted">Emme tee osoitteellasi mitään laitonta.</small> 
+						 value={this.state.email} onChange={this.onChange}/> */}	
 			</div>
 			<p>Salasanan vaihtaminen</p>
 			<div class="form-group">
